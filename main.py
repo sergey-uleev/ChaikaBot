45a1be5e93a5da4817136bb2818a810cbd94d9b4
Sergey <mazi77@bk.ru>
2022-11-01 15:36:42 +0500

    добавлены кнопки

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from typing import Any
 import telebot
+from telebot import types
+import requests
+from bs4 import BeautifulSoup as b
 
 bot = telebot.TeleBot('5475021171:AAEkYwe-tgUwWdbcQGarL4FpQSqIDAAM2KY')
 
@@ -9,9 +13,20 @@ def start(message):
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
-@bot.message_handler()
-def get_user_text(message):
-    bot.send_message(message.chat.id, message, parse_mode='html')
+@bot.message_handler(commands=['website'])
+def website(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Посетить веб сайт", url="https://itproger.com/"))
+    bot.send_message(message.chat.id, 'Передите на сайт', reply_markup=markup)
+
+
+@bot.message_handler(commands=['help'])
+def website(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    start = types.KeyboardButton('start')
+    website = types.KeyboardButton('website')
+    markup.add(start, website)
+    bot.send_message(message.chat.id, 'Передите на сайт', reply_markup=markup)
 
 
 bot.polling(none_stop=True)
